3c562a2ad2d93cbd1184ef5262357081ce64e7ac
Josh <yr18478@bristol.ac.uk>
2022-12-30 23:59:31 +0000

    Added button to generate square

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,12 +3,17 @@ import React from 'react'
 interface buttonProps {
     onClick: (() => void) | ((e: React.MouseEvent) => void);
     text: string;
+    className: string;
 }
 
-function Button({ onClick, text }: buttonProps) {
+function Button({ onClick, text, className }: buttonProps) {
     return (
-        <button onClick={onClick} className='button'>{text}</button>
+        <button onClick={onClick} className={`button ${className}`}>{text}</button>
     )
+}
+
+Button.defaultProps = {
+    className: '',
 }
 
 export default Button
--- src/pages/mainGame.tsx
+++ src/pages/mainGame.tsx
@@ -6,43 +6,44 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SquareState from '../interfaces/squareState';
 import HelpHover from '../components/helpHover';
+import Button from '../components/button';
 
 function MainGame() {
   const dispatch = useDispatch();
   const grid = useSelector((state: PirateState) => state.grid);
 
-  useEffect(() => {
-    function nextSquare() {
-      let x: number, y: number;
-  
-      const flattened = grid.flat();
-  
-      const emptyCount = flattened.map(
-        e => e === SquareState.Empty ? 1 : 0
-      ).reduce((a: number, b: number) => a + b, 0);
-  
-      if (emptyCount === 0) {
-        return;
-      }
-  
-      const idx = Math.floor(Math.random() * emptyCount);
-      let passed = 0;
-  
-      for (x = 0; x < grid.length; x++) {
-        for (y = 0; y < grid[x].length; y++) {
-          if (grid[x][y] === SquareState.Empty) {
-            if (passed === idx) {
-              dispatch(setRecentSquare({ x: x, y: y }));
-              return;
-            }
-            passed++;
-          }
-        }
-      }
-  
-      console.error("UNREACHABLE");
+  function nextSquare() {
+    let x: number, y: number;
+
+    const flattened = grid.flat();
+
+    const emptyCount = flattened.map(
+      e => e === SquareState.Empty ? 1 : 0
+    ).reduce((a: number, b: number) => a + b, 0);
+
+    if (emptyCount === 0) {
+      return;
     }
 
+    const idx = Math.floor(Math.random() * emptyCount);
+    let passed = 0;
+
+    for (x = 0; x < grid.length; x++) {
+      for (y = 0; y < grid[x].length; y++) {
+        if (grid[x][y] === SquareState.Empty) {
+          if (passed === idx) {
+            dispatch(setRecentSquare({ x: x, y: y }));
+            return;
+          }
+          passed++;
+        }
+      }
+    }
+
+    console.error("UNREACHABLE");
+  }
+
+  useEffect(() => {
     function handleKeyDown(e: KeyboardEvent) {
       if (e.key === 'Enter') {
         if (e.target instanceof Element) {
@@ -67,7 +68,10 @@ function MainGame() {
   return (
     <div className="Game">
       <div className='infoCol'>
-        <HelpHover />
+        <div className='lowerContainer'>
+          <HelpHover />
+          <Button onClick={() => nextSquare()} text='Next' className='nextBtn' />
+        </div>
         <CordDisplay />
         <History />
       </div>
