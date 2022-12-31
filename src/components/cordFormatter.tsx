2a9271ef758d6696fe085b5871c59a58bc86c117
Josh <yr18478@bristol.ac.uk>
2022-12-31 12:48:31 +0000

    Monospaced coordinate font

--- src/components/cordFormatter.tsx
+++ src/components/cordFormatter.tsx
@@ -1,14 +1,19 @@
-import { xyPos } from '../features/pirateSlice'
+import { useSelector } from 'react-redux';
+import { PirateState, xyPos } from '../features/pirateSlice'
 
 interface CordFormatterProps extends xyPos {}
 
 function CordFormatter({x, y}: CordFormatterProps) {
+  const gridSize: number = useSelector((state: PirateState) => state.gridSize);
   if (x === undefined || y === undefined) {
     return <></>
   }
+
+  const length: number = Math.floor(Math.log10(gridSize))+1;
+
   const char = String.fromCharCode(x+65);
   return (
-    <>{char}{y+1}</>
+    <span className='monospace'>{char}{(y+1).toString().padStart(length, '0')}</span>
   )
 }
 
